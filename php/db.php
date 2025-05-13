2ba290e47bf6d4d8452bb73d4f8bfc58187b7c58
Plugner <48152341+Plugner@users.noreply.github.com>
2025-05-13 09:45:40 -0300

    Update database credentials to use ini_get() function

--- php/db.php
+++ php/db.php
@@ -2,8 +2,8 @@
 
     $db_name = "nextrain_production";
     $db_user = "nextrain";
-    $db_pass = "nextrain";
-    $db_host = "179.222.224.43";
+    $db_pass = ini_get('nt_db_pass');
+    $db_host = ini_get('nt_db_host');
     $db_port = 6306;
 
     $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
