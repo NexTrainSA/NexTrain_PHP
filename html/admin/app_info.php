f8bf2c1427ffbbfaed25dc9bb1e0426e8a39d072
Plugner <48152341+Plugner@users.noreply.github.com>
2025-05-20 08:46:47 -0300

    Add styling and display app information in app_info.php

---
+++ html/admin/app_info.php
@@ -0,0 +1,21 @@
+<style>
+    body {
+        background-color: #dbd4bf !important;
+    }
+</style>
+
+<?php
+
+    include_once("./db.php");
+    $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
+    $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
+    $commitDate->setTimezone(new \DateTimeZone('America/Sao_Paulo'));
+    $commitAuthor = trim(exec('git log -n1 --pretty=%cn HEAD'));
+
+    echo "<br/><h1>Informações do App</h1>";
+    echo "<h2>Versão: " . $commitHash . "</h2>";
+    echo "<h2>Data do último commit: " . $commitDate->format('d/m/Y H:i:s') . "</h2>";
+    echo "<h2>Autor do último commit: " . $commitAuthor . "</h2>";
+
+    phpinfo();
+?>
