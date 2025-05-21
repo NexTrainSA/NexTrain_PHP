fea44230a90d3f74069aaeaa4dc83ecccf4e3472
Plugner <48152341+Plugner@users.noreply.github.com>
2025-05-21 08:42:11 -0300

    Fix styling issues and update links in header

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,7 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "400px";
+    document.getElementById("mySidenav").style.width = "30%";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0%";
 }
