ce67d308c8f0f6265bae86ca05cbc09e9a6b1ae1
Plugner <48152341+Plugner@users.noreply.github.com>
2025-06-04 10:13:02 -0300

    Update styles, implement responsive sidebar, and enhance dark mode functionality
    Co-authored-by: Yasmin Victhória da Silva <Yasmindasilvaa@users.noreply.github.com>

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,3 +1,30 @@
+
+// https://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
+}
+
+window.onload(() => {
+    console.log("load");
+})
+
+if(getCookie("darkMode") === "true") {
+    toggleDarkMode();
+    console.log("Dark Mode")
 }
