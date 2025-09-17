57d53952bc724f7c8b4e9dab164b66a208c293fd
Yasmindasilvaa <yasmin_silva1@estudante.sesisenai.org.br>
2025-09-17 09:54:13 -0300

    Atualizações - 17-09

--- php/db.php
+++ php/db.php
@@ -1,72 +1,91 @@
 <?php
-    $inipath = php_ini_loaded_file();
-    $ini_array = parse_ini_file($inipath , true)["PHP"];
+$inipath = php_ini_loaded_file();
+$ini_array = parse_ini_file($inipath, true)["PHP"];
 
-    $db_name = "nextrain_production";
-    $db_user = "nextrain";
-    $db_pass = $ini_array["nt_db_pass"];
-    $db_host = $ini_array["nt_db_host"];
-    $db_port = 6306;
+$db_name = "nextrain_production";
+$db_user = "nextrain";
+$db_pass = $ini_array["nt_db_pass"];
+$db_host = $ini_array["nt_db_host"];
+$db_port = 6306;
 
-    $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
 
-    if(!$con) {
-        die("Connection failed: " . mysqli_connect_error());
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+function get_id_from_username($username)
+{
+    global $con;
+    $query = "SELECT id_usuario FROM usuario WHERE username_usuario = '$username'";
+    $result = mysqli_query($con, $query);
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        return $row['id_usuario'];
     }
+    return null;
+}
 
-    function get_id_from_username($username) {
-        global $con;
-        $query = "SELECT id_usuario FROM usuario WHERE username_usuario = '$username'";
-        $result = mysqli_query($con, $query);
-        if ($result) {
-            $row = mysqli_fetch_assoc($result);
-            return $row['id_usuario'];
-        }
-        return null;
+function get_permission_id_by_name($permissionName)
+{
+    global $con;
+    $query = "SELECT id_permissao FROM permissao WHERE nome_permissao = '$permissionName'";
+    $result = mysqli_query($con, $query);
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        return $row['id_permissao'];
     }
+    return null;
+}
 
-    function get_permission_id_by_name($permissionName) {
-        global $con;
-        $query = "SELECT id_permissao FROM permissao WHERE nome_permissao = '$permissionName'";
-        $result = mysqli_query($con, $query);
-        if ($result) {
-            $row = mysqli_fetch_assoc($result);
-            return $row['id_permissao'];
-        }
-        return null;
+function get_username_from_id($id)
+{
+    global $con;
+    $query = "SELECT username_usuario FROM usuario WHERE id_usuario = '$id'";
+    $result = mysqli_query($con, $query);
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        return $row['username_usuario'];
     }
+    return null;
+}
 
-    function get_username_from_id($id) {
-        global $con;
-        $query = "SELECT username_usuario FROM usuario WHERE id_usuario = '$id'";
-        $result = mysqli_query($con, $query);
-        if ($result) {
-            $row = mysqli_fetch_assoc($result);
-            return $row['username_usuario'];
-        }
-        return null;
+function check_user_permission($username, $permission)
+{
+    global $con;
+    $id = get_id_from_username($username);
+    $idperm = get_permission_id_by_name($permission);
+
+    $q = mysqli_query($con, "SELECT id_permissao FROM permissao_usuario WHERE id_usuario_permissao = '$id' AND id_permissao = '$idperm'");
+    if (mysqli_num_rows($q) > 0) {
+        return true;
     }
+}
 
-    function check_user_permission($username, $permission) {
-        global $con;
-        $id = get_id_from_username($username);
-        $idperm = get_permission_id_by_name($permission);
-
-        $q = mysqli_query($con, "SELECT id_permissao FROM permissao_usuario WHERE id_usuario_permissao = '$id' AND id_permissao = '$idperm'");
-        if(mysqli_num_rows($q) > 0) {
-            return true;
-        }
+function get_all_users_as_array()
+{
+    global $con;
+    $q = mysqli_query($con, "SELECT * FROM usuario");
+    $users = [];
+    while ($row = mysqli_fetch_assoc($q)) {
+        $users[] = $row;
     }
+    return $users;
+}
 
-    function get_all_users_as_array() {
-        global $con;
-        $q = mysqli_query($con, "SELECT * FROM usuario");
-        $users = [];
-        while($row = mysqli_fetch_assoc($q)) {
-            $users[] = $row;
-        }
-        return $users;
+function create_requests_table()
+{
+    global $con;
+    $sql = "CREATE TABLE IF NOT EXISTS maintenance_requests (
+        id_funcionário INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )";
+    if (mysqli_query($con, $sql)) {
+        return true;
+    } else {
+        return "Erro: " . mysqli_error($con);
     }
-    
-    return false;
-?>
+}
+
+return false;
