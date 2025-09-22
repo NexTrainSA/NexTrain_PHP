79b9df84ac39b24319e02b36a40600280f69870c
Yasmindasilvaa <yasmin_silva1@estudante.sesisenai.org.br>
2025-09-22 10:14:25 -0300

    Atualiza cores do modo escuro e ajusta a tabela de agendamentos no banco de dados

--- php/db.php
+++ php/db.php
@@ -100,16 +100,9 @@ function create_schedule_table()
 {
     global $con;
     $sql = "CREATE TABLE IF NOT EXISTS schedule (
-         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        nome_funcionario VARCHAR(100) NOT NULL,
-        cpf_funcionario VARCHAR(11) NOT NULL,
-        id_funcionario INT NOT NULL,
-        telefone_funcionario VARCHAR(11),
-        info_trem VARCHAR(100),
-        descricao_problema TEXT NOT NULL,
-        tecnico_responsavel VARCHAR(100),
-        data_entrada DATE  NOT NULL,
-        data_saida DATE NOT NULL,
+        id_tarefa INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        fk_funcionario INT NOT NULL,
+        descricao_tarefa TEXT NOT NULL,
         FOREIGN KEY (id_funcionario) REFERENCES usuario(id_usuario)
     )";
     if (mysqli_query($con, $sql)) {
