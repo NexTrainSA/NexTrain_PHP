70a45c55f086d135293517a1b55f5792685bcd14
Fernanda788 <fernanda_anna@estudante.sesisenai.org.brm>
2025-10-10 11:19:39 -0300

    Revert "Co-authored-by: Gabriela166 <Gabriela166@users.noreply.github.com>"
    This reverts commit 0c59baa9b8ba7e7e4e53cc926549a70c58b88466.

--- php/insert_task.php
+++ php/insert_task.php
@@ -4,10 +4,10 @@ require_once('db.php');
 
 $ID_FUNCIONARIO = $_POST['id_funcionario'] ?? '';
 $DESCRICAO_PROBLEMA = $_POST['descricao_tarefa'] ?? '';
-$DATA = $_POST['data'];
 
-$stmt = $con->prepare("INSERT INTO agenda (id_funcionario, descricao_tarefa, data_tarefa) VALUES(?, ?, ?)");
-$stmt->bind_param("is", $ID_FUNCIONARIO, $DESCRICAO_TAREFA, $DATA_TAREFA);
+
+$stmt = $con->prepare("INSERT INTO agenda (id_funcionario, descricao_tarefa) VALUES(?, ? )");
+$stmt->bind_param("is", $ID_FUNCIONARIO, $DESCRICAO_TAREFA);
 
 if ($stmt->execute()) {
 } else {
--- php/listar_trem.php
+++ php/listar_trem.php
@@ -2,7 +2,7 @@
 
 require_once('db.php');
 
-$stmt = $con->prepare("SELECT id_trem, nome_trem FROM trens");
+$stmt = $con->prepare("SELECT id_trem, nome_trem FROM trains");
 $stmt->execute();
 $resultado = $stmt->get_result();
 
