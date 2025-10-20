f2f9bcddb84e58911b25b672fe532ce8fae3a6f1
João Rodrigo H. Luckow <48152341+Plugner@users.noreply.github.com>
2025-10-20 12:20:10 -0300

    jQuery debugging

--- js/graph_relations_updater.js
+++ js/graph_relations_updater.js
@@ -4,7 +4,8 @@ traj.addEventListener('change',function(){
     alert('changed - new value: ' + traj.value);
 
     $.get( "php/listar_arestas_estacao?id_estacao=" + traj.value, function( data ) {
-        $( ".result" ).html( data );
-        alert( "Load was performed." );
+        var json = $.parseJSON(data);
+        alert(json);
+        console.log(json);
     });
 });
