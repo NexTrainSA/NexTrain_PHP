73ccf45f5f7e87710e8be3114d85778761fc70ea
Annatnat <fernanda.ribeiro.santanna@gmail.com>
2025-10-28 16:18:22 -0300

    Voltando atrás porque não deu nada certo

--- php/excluir_estacao.php
+++ php/excluir_estacao.php
@@ -6,11 +6,9 @@ $codigo = $_GET['id'];
 
 $stmt = $con->prepare("DELETE FROM estacao WHERE id_estacao = ?");
 $stmt->bind_param("i", $codigo);
-$redirect = $_GET['redirect'] ?? 'stations.php';
-
 
 if ($stmt->execute()) {
-    header("Location: $redirect");
+    header("Location: stations.php");
     exit;
 } else {
     echo "Erro ao excluir a estação: " . $stmt->error;
