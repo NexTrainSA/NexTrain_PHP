fcc6f51a0e66c55fdf32d0e5bc5813eebd1bc6d9
Annatnat <fernanda.ribeiro.santanna@gmail.com>
2025-10-28 17:34:22 -0300

    Alterações finais

--- php/alerts.php
+++ php/alerts.php
@@ -94,8 +94,8 @@
                                     <md-icon class="route-icon">train</md-icon>
                                 </div>
                                 <div class="route-info">
-                                    <h3 class="route-name"><?= htmlspecialchars($alertas['nome_destinatario']) ?></h3>
-                                    <p class="route-line"><?= htmlspecialchars($alertas['nome_remetente']) ?></p>
+                                    <h3 class="route-name"><?= htmlspecialchars($alertas['nome_remetente']) ?></h3>
+                                    <p class="route-line"><?= htmlspecialchars($alertas['nome_destinatario']) ?></p>
                                 </div>
                                 <md-chip label="Ativo" class="status-chip status-on-time">
                                     <md-icon slot="icon">check_circle</md-icon>
